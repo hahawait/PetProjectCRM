b75b3d608683915d2bf03dba521978bee4ed1ef9
hahawait <raenkoag24@gmail.com>
2023-06-24 23:16:47 +0400

    add core app, templates

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from accounts.models import UserRole
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            role = request.POST.get('role')
+            user_role = UserRole(user=user, role=role)
+            user_role.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
