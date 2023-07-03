8cff469c3bd3a10c8c23dbfa32738967fd1ad579
hahawait <raenkoag24@gmail.com>
2023-07-03 21:39:16 +0400

    add contact views and templates, refactor

--- server/companies/views.py
+++ server/companies/views.py
@@ -1,10 +1,11 @@
+from django.views import View
 from django.db.models import Q
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 
-from companies.models import Company
-from companies.forms import CompanyForm
+from companies.models import Company, Contact
+from companies.forms import CompanyForm, ContactForm
 from companies.utils import check_user_company
 
 
@@ -28,6 +29,7 @@ def company_list(request):
 
 def company_detail(request, company_id):
     '''Рендерит шаблон компании'''
+
     if request.method == 'GET':
         company = get_object_or_404(Company, pk=company_id)
         return render(request, 'companies/company_detail.html', {'company': company})
@@ -36,11 +38,10 @@ def company_detail(request, company_id):
 @login_required
 def company_create(request):
     '''Создает объект компании'''
+
     if request.method == 'GET':
-        context = {
-            'form' : CompanyForm
-        }
-        return render(request, 'companies/company_create.html', context)
+        return render(request, 'companies/company_create.html', {'form' : CompanyForm})
+
     if request.method == 'POST':
         form = CompanyForm(request.POST)
         if form.is_valid():
@@ -55,24 +56,93 @@ def company_create(request):
 
 
 @login_required
-def company_edit(request):
+def company_update(request):
     '''Изменяет существующую компанию пользователя'''
+
     company = get_object_or_404(Company, user=request.user)
     if request.method == 'GET':
         form = CompanyForm(instance=company)
-        # context = {
-        #     'form': form,
-        #     'company': company
-        # }
-        # return render(request, 'companies/company_edit.html', context)
+
     if request.method == 'POST':
         form = CompanyForm(request.POST, instance=company)
         if form.is_valid():
             form.save()
             return redirect('company-list')
 
-    context = {
-        'form': form,
-        'company': company
-    }
-    return render(request, 'companies/company_edit.html', context)
+    return render(request, 'companies/company_update.html', {'form': form})
+
+
+class ContactListView(View):
+    '''Список контактных лиц компании'''
+
+    def get(self, request, company_id):
+        '''Рендерит шаблон со списком контактных лиц компании'''
+
+        queryset = get_list_or_404(Contact, company__id=company_id)
+        return render(request, 'companies/contact_list.html', {'contact_list': queryset})
+
+
+class ContactDetailView(View):
+    '''Контактное лицо компании'''
+
+    def get(self, request, company_id, slug):
+        '''Рендерит шаблон с контактным лицом компании'''
+
+        contact = get_object_or_404(Contact, company__id=company_id, slug=slug)
+        return render(request, 'companies/contact_detail.html', {'contact': contact})
+
+
+class ContactCreateView(View):
+    '''Создание контактного лица компании'''
+
+    def get(self, request, company_id):
+        '''Рендерит шаблон с формой, если пользователь - создатель компании'''
+
+        company = Company.objects.select_related('user').get(pk=company_id)
+        if request.user != company.user:
+            return HttpResponse('Вы можете добавить контактное лицо только своей компании')
+        return render(request, 'companies/contact_create.html', {'form': ContactForm})
+
+
+    def post(self, request, company_id):
+        '''Добавляет контактное лицо, если пользователь - создатель компании'''
+
+        company = Company.objects.select_related('user').get(pk=company_id)
+        if request.user != company.user:
+            return HttpResponse('Вы можете добавить контактное лицо только своей компании')
+        else:
+            form = ContactForm(request.POST)
+            if form.is_valid():
+                contact = form.save(commit=False)
+                contact.company = company
+                contact.save()
+                return redirect('contact-list', company_id)
+
+
+class ContactUpdateView(View):
+    '''Изменение контактного лица'''
+
+    def get(self, request, company_id, slug):
+        '''Получение формы, если пользователь - создатель объекта компании'''
+
+        contact = get_object_or_404(Contact, company__id=company_id,
+                                    slug=slug, company__user=request.user)
+        form = ContactForm(instance=contact)
+        return render(request, 'companies/contact_update.html', {'form': form})
+
+
+    def post(self, request, company_id, slug):
+        '''Изменение существующего контакного лица'''
+
+        company = Company.objects.select_related('user').get(pk=company_id)
+        if request.user != company.user:
+            return HttpResponse('Вы можете изменить контактное лицо только своей компании')
+        else:
+            contact = get_object_or_404(Contact, company__id=company_id,
+                                        slug=slug, company__user=request.user)
+            form = ContactForm(request.POST, instance=contact)
+            if form.is_valid():
+                contact = form.save(commit=False)
+                contact.company = company
+                contact.save()
+                return redirect('contact-list', company_id)
